bbb445f4c58838cf1de7bb689c3265665acb9a5f
Torkil Moe <torkil.moe@trondheim.kommune.no>
2021-10-16 21:57:12 +0200

    doit

--- app/authConfig.js
+++ app/authConfig.js
@@ -2,7 +2,8 @@ const msalConfig = {
     auth: {
         clientId: "9a71cda0-f2a3-4224-8a9d-445eea62a8e7",
         authority: "https://login.microsoftonline.com/831195d3-b68b-433a-8687-4cdb1532958e",
-        redirectUri: "http://localhost:3000",
+        // redirectUri: "http://localhost:3000",
+        redirectUri: "https://proud-stone-024659c03.azurestaticapps.net/",
     },
     cache: {
         cacheLocation: "sessionStorage", // This configures where your cache will be stored
