310aa9b3b3ff76cadabfcf9ed332ba09345a412d
Torkil Moe <torkil.moe@trondheim.kommune.no>
2021-10-17 07:10:46 +0200

    oo

--- app/graph.js
+++ app/graph.js
@@ -19,6 +19,10 @@ function callMSGraph(endpoint, token, callback) {
         .catch(error => console.log(error));
 }
 
+function isEmptyOrSpaces(str) {
+    return str === null || str.match(/^ *$/) !== null;
+}
+
 // function callMSFunc(endpoint, token, callback,msg,to) {
 function callMSFunc(endpoint, token, callback) {
     const headers = new Headers();
@@ -29,10 +33,15 @@ function callMSFunc(endpoint, token, callback) {
     headers.append("Content-type", "application/json; charset=UTF-8");
     // headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
 
+    var reciever = document.getElementById("lto").value;
+    var msg = document.getElementById("lmsg").value;
+
+
+
     let _data = {
-        target: '41439162',
-        msg: 'vicenajs',
-        sender: 'kjærest'
+        target: document.getElementById("lto").value,
+        msg: document.getElementById("lmsg").value,
+        sender: username
     }
 
     const options = {
@@ -44,11 +53,20 @@ function callMSFunc(endpoint, token, callback) {
 
 
     // data to be sent to the POST request
+    if (!isEmptyOrSpaces(reciever)) {
+        if (!isEmptyOrSpaces(msg)) {
+            console.log('request made to Graph API at: ' + new Date().toString());
+            console.log('Body: ' + (JSON.stringify(_data)));
+            fetch(endpoint, options)
+                .then(response => response.json())
+                .then(json => console.log(json))
+                .catch(error => console.log(error));
 
-    console.log('request made to Graph API at: ' + new Date().toString());
-    console.log('Body: ' + (JSON.stringify(_data)));
-    fetch(endpoint, options)
-        .then(response => response.json())
-        .then(json => console.log(json))
-        .catch(error => console.log(error));
+                document.getElementById("lmsg").value = '';
+                reciever = document.getElementById("lto").value = '';
+            
+        }
+    }
+
+
 }
